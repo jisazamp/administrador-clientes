64f0ccc578ff31f4c9bdfbad3aa86e842f6eef5a
Juan P. Isaza <jisazamp@gmail.com>
2022-06-21 11:19:11 -0500

    Basic navbar

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,12 +1,38 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
 
 const Layout = () => {
-  return (
-    <>
-      <div>Layout</div>
+  const location = useLocation();
+  const currentUrl = location.pathname;
 
-      <Outlet />
-    </>
+  return (
+    <div className='md:flex md:min-h-screen'>
+      <div className='md:w-1/4 bg-[#1f2f56] px-5 py-10'>
+        <h2 className='text-[#e0e8ff] text-4xl text-center font-bold'>
+          CRM - Clientes
+        </h2>
+        <nav className='mt-10'>
+          <Link
+            className={`nav-link ${
+              currentUrl === '/clients' && 'nav-link__active'
+            }`}
+            to='/clients'
+          >
+            Clientes
+          </Link>
+          <Link
+            className={`nav-link ${
+              currentUrl === '/clients/new' && 'nav-link__active'
+            }`}
+            to='/clients/new'
+          >
+            Nuevo cliente
+          </Link>
+        </nav>
+      </div>
+      <div className='md:w-3/4'>
+        <Outlet />
+      </div>
+    </div>
   );
 };
 
